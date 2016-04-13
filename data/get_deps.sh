f51ebe1c0ab3748c329b134eeee12383e385385b
Andrea Frittoli <afrittoli@users.noreply.github.com>
2016-04-13 22:41:36 +0100

    Fix input data, work on graphs
    Fixed and improved the script to collect data, now collecting
    both the # of repos with 1+ import as well as the total number of
    imports across repos.
    
    Working on graphs, various options in excel.

--- data/get_deps.sh
+++ data/get_deps.sh
@@ -7,13 +7,15 @@ not_ignore_case='i=Nope'
 python_files='files=.*\.py$'
 query="^(import|from) tempest"
 
-# Get the ~raw data
-curl -s --get ${code_search} \
-        --data-urlencode ${all_repos} \
-        --data-urlencode ${not_ignore_case} \
-        --data-urlencode ${python_files} \
-        --data-urlencode "q=$query" |
-    jq '.Results | del(.["tempest-lib"]) | del(.tempest) ' > ${script_folder}/raw/data.json
+# Get the ~raw data. Delete the cache to fetch a new version
+if [ ! -f ${script_folder}/raw/data.json ]; then
+    curl -s --get ${code_search} \
+            --data-urlencode ${all_repos} \
+            --data-urlencode ${not_ignore_case} \
+            --data-urlencode ${python_files} \
+            --data-urlencode "q=$query" |
+        jq '.Results | del(.["tempest-lib"]) | del(.tempest) ' > ${script_folder}/raw/data.json
+fi
 
 # Build the list of repos
 echo "Building the list of repos in ${script_folder}/repos"
@@ -40,16 +42,23 @@ for repo in $(cat ${script_folder}/repos); do
     printf "\n"
 done >> ${script_folder}/imports_per_repo
 
+# Build list of all lines for full count
+cat ${script_folder}/raw/data.json | \
+    jq -r '.[].Matches | .[].Matches | .[].Line' | \
+    sed -e 's,[ ]*as .*,,' -e 's,[ ]*# .*,,' -e 's,from ,,g' -e 's, import ,.,g' -e 's,^import ,,g' -e 's,tempest.lib,tempest#lib,g'| \
+    cut -d'.' -f1,2 | sed -e 's,tempest#lib,tempest.lib,g' > ${script_folder}/raw/all_lines
+
 # Build the repo per imports tables
 # Tempest first and tempest.lib then
 for filter in 'tempest' 'lib'; do
     [[ "${filter}" == "tempest" ]] && grep_opt='-v' || grep_opt=' '
     echo "Building the table of repos per import in ${script_folder}/repo_per_${filter}_imports"
-    echo "import;namespace;count;repos" > ${script_folder}/repo_per_${filter}_imports
+    echo "import;namespace;count;total_count;repos" > ${script_folder}/repo_per_${filter}_imports
     for import in $(cat ${script_folder}/imports | egrep ${grep_opt} '^tempest.lib'); do
         printf "${import};${import#tempest.*};"
-        printf "$(grep -c ${import} ${script_folder}/imports_per_repo);"
-        printf "$(grep ${import} ${script_folder}/imports_per_repo | cut -d';' -f1 | tr '\n' ',' | sed -e 's/,$//')"
+        printf "$(egrep -c ${import}'(,|$)' ${script_folder}/imports_per_repo);"
+        printf "$(egrep -c ${import} ${script_folder}/raw/all_lines);"
+        printf "$(egrep ${import}'(,|$)' ${script_folder}/imports_per_repo | cut -d';' -f1 | tr '\n' ',' | sed -e 's/,$//')"
         printf "\n"
     done >> ${script_folder}/repo_per_${filter}_imports
 done
